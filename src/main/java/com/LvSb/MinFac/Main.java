37dec8105e82088491cb96ca5658f1eb4cee8d99
Patrick Rakowicz <lividsubset3@gmail.com>
2021-05-10 01:48:00 -0600

    ++

--- src/main/java/com/LvSb/MinFac/Main.java
+++ src/main/java/com/LvSb/MinFac/Main.java
@@ -1,9 +1,18 @@
 package com.LvSb.MinFac;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.Logger;
+
+import java.util.logging.LogManager;
+
 
 @Mod(Main.MODID)
 public final class Main {
 
     public static final String MODID = "minfac";
+    public static final Logger LOGGER = (Logger) LogManager.getLogManager().getLogger(MODID);
+
+    public Main () {
+        LOGGER.debug("Hello From Mine Factorization!")
+    }
 }
