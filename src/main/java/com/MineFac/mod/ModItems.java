5bb2a8041f45ea9a2004664e5c9c8f15bb8f631b
Patrick Rakowicz <lividsubset3@gmail.com>
2021-05-10 00:08:49 -0600

    Test One

---
+++ src/main/java/com/MineFac/mod/ModItems.java
@@ -0,0 +1,14 @@
+package com.MineFac.mod;
+
+
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod("BlockMod")
+public class ModItems {
+
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+}
--- src/main/java/com/MineFac/mod/init/ModItems.java
+++
@@ -1,8 +0,0 @@
-package com.MineFac.mod.init;
-
-
-
-public class ModItems {
-
-}
-    
