19865797e16447d9c15233baf6d6ca40922d81e5
Patrick Rakowicz <lividsubset3@gmail.com>
2021-05-13 21:33:24 -0600

    small fix on hexamite and hexamine

--- src/main/java/com/LvSb/MinFac/lists/ItemList.java
+++ src/main/java/com/LvSb/MinFac/lists/ItemList.java
@@ -17,8 +17,8 @@ public class ItemList {
 
     public static final RegistryObject<Item> NEW_EGG = ITEMS.register("new_egg",
             () -> new Item(new Item.Properties().group(ModItemGroup.MINFAC_TAB)));
-    
-    public static final RegistryObject<Item> REFINED_HEXAMITE = ITEMS.register("refined_hexamite",
+
+    public static final RegistryObject<Item> REFINED_HEXAMITE = ITEMS.register("hexamite",
             () -> new Item(new Item.Properties().group(ModItemGroup.MINFAC_TAB)));
 
     public static final RegistryObject<Item> MANURE = ITEMS.register("manure",
